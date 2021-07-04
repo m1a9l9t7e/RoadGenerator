9aa4368ea2c1879e6470965a2e5805105f707b02
malte <m.schoebel@tu-bs.de>
2021-07-04 14:36:53 +0200

    Add comments for util.py

--- util.py
+++ util.py
@@ -3,13 +3,22 @@ import math
 from anim_sequence import AnimationObject
 
 
-class Converter:
+#######################
+##### GRAPH STUFF #####
+#######################
+
+class GraphTour:
+    """
+    This class converts a given graph containing a single cycle to a tour.
+    The tour can be retrieved as a sequence of nodes or edges.
+    """
     def __init__(self, graph, square_size, track_width):
         self.graph = graph
         self.square_size = square_size
         self.track_width = track_width
         self.nodes = list()
         self.edges = list()
+        self.extract_tour()
 
     def extract_tour(self):
         grid = self.graph.grid
@@ -31,8 +40,27 @@ class Converter:
         self.nodes = tour
         # print("Found tour with {} nodes and {} edges!".format(len(self.nodes), len(self.edges)))
 
+    def get_nodes(self):
+        """
+        :returns tour as sequence of nodes
+        """
+        return self.nodes
+
+    def get_edges(self):
+        """
+        :returns tour as sequence of edges
+        """
+        return self.edges
+
 
 def get_next_node(node, previous_node=None):
+    """
+    Given two nodes, node1 and node2, return any adjacent node to node1 that is not node2.
+    Assuming that node1 has degree2, this must return a node leading away from node1 and node2.
+    :param node: node1
+    :param previous_node: node2
+    :returns node that is adjacent to node1 but is not node2
+    """
     if node.get_degree() != 2:
         raise ValueError("Graph not 2 factorized!")
 
@@ -44,7 +72,44 @@ def get_next_node(node, previous_node=None):
         return node2
 
 
+def get_adjacent(grid, coords):
+    """
+    Get list of booleans indicating which adjacent cells given coords have in a given grid.
+    :returns list of booleans: [right, up, left, down]
+    """
+    x, y = coords
+    adjacent = []
+    for _x, _y in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+        if x + _x >= len(grid) or y + _y >= len(grid[0]) or x + _x < 0 or y + _y < 0:
+            adjacent.append(False)
+        else:
+            adjacent.append(grid[x + _x][y + _y] > 0)
+
+    return adjacent
+
+
+def is_adjacent(coords1, coords2):
+    """
+    Check if two coordinates are adjacent, based only on coordinate values.
+    :returns True, if manhattan distance between coords1 and coords2 == 1
+    TODO: implement manhattan distance instead of for loop
+    """
+    x1, y1 = coords1
+    x2, y2 = coords2
+    for _x, _y in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+        if x1 + _x == x2 and y1 + _y == y2:
+            return True
+    return False
+
+
+#######################
+##### MANIM STUFF #####
+#######################
+
 class Grid:
+    """
+    Creates a drawable grid, consisting of manim lines
+    """
     def __init__(self, graph, square_size, shift, color=GREEN_C, stroke_width=0.4):
         self.graph = graph
         self.width = len(graph.grid)
@@ -86,112 +151,6 @@ class Grid:
         return animation_sequence
 
 
-def transform_coords(coords, shift, scale):
-    x, y = coords
-    x_shift, y_shift = shift
-    return x_shift + x * scale, y_shift + y * scale, 0
-
-
-class TrackPoint:
-    def __init__(self, coords, direction):
-        self.coords = coords
-        self.direction = direction / np.linalg.norm(direction)
-
-    def __str__(self):
-        return "coords: {}, direction: {}".format(self.coords, self.direction)
-
-    def alter_direction(self, angle):
-        """
-        alter direction by adding angle in radian
-        :param angle:
-        :return:
-        """
-        current_angle = np.arctan2(*self.direction)
-        new_angle = current_angle + angle
-        direction = [np.sin(new_angle), np.cos(new_angle)]
-        self.direction = np.round(direction, decimals=2)
-
-    def as_list(self):
-        x, y, _ = self.coords
-        dx, dy = self.direction
-        return [x, y, dx, dy]
-
-
-class GridShowCase:
-    def __init__(self, num_elements, element_dimensions, spacing=[1, 1], space_ratio=[16, 9]):
-        self.grid_dimensions = calculate_grid_dimensions(num_elements, ratio=space_ratio)
-        self.element_width, self.element_height = element_dimensions
-        self.x_spacing, self.y_spacing = spacing
-
-    def get_global_camera_settings(self):
-        cols, rows = self.grid_dimensions
-        width = cols * self.element_width + (cols - 1) * self.x_spacing
-        height = rows * self.element_height + (rows - 1) * self.y_spacing
-        size = [width, height]
-        position = np.array(size) / 2
-        return position, size
-
-    def get_zoomed_camera_settings(self, index):
-        bottom_left_x, bottom_left_y = self.get_element_coords(index)
-        position = (bottom_left_x + self.element_width/2, bottom_left_y + self.element_height/2)
-        size = [self.element_width, self.element_height]
-        return position, size
-
-    def get_element_coords(self, index):
-        cols, rows = self.grid_dimensions
-        x = index % cols
-        y = np.floor(index / cols)
-        bottom_left_x = x * (self.element_width + self.x_spacing)
-        bottom_left_y = y * (self.element_height + self.y_spacing)
-        return bottom_left_x, bottom_left_y
-
-
-def calculate_grid_dimensions(num_elements, ratio):
-    factor = (ratio[0] * ratio[1])
-    scale = math.sqrt(num_elements / factor)
-    grid_dims = [np.ceil(ratio[0] * scale), np.ceil(ratio[1] * scale)]
-    approx = ratio[0] * scale * ratio[1] * scale
-    # print("Arrange {} elements in ratio {} x {}!".format(num_elements, *ratio))
-    # print("scaling is {scale:.2f}! Because {x_scaled:.2f} x {y_scaled:.2f} = {approx:.2f} ~ {num}".format(scale=scale, x_scaled=scale * ratio[0],
-    #                                                                                                       y_scaled=scale * ratio[1], num=num_elements,
-    #                                                                                                       approx=approx))
-    print("Approximate grid dimensions: {} x {}".format(*grid_dims))
-    return grid_dims
-
-
-def draw_graph(graph):
-    animation_sequence = []
-    node_drawables = [FadeIn(node.drawable) for node in graph.nodes]
-    edge_drawables = [Create(edge.drawable) for edge in graph.edges]
-    animation_sequence.append(AnimationObject(type='play', content=node_drawables, duration=1, bring_to_front=True))
-    animation_sequence.append(AnimationObject(type='play', content=edge_drawables, duration=1, bring_to_back=True))
-    return animation_sequence
-
-
-def add_graph(graph):
-    nodes = [node.drawable for node in graph.nodes]
-    edges = [edge.drawable for edge in graph.edges]
-    return [AnimationObject(type='add', content=nodes),
-            AnimationObject(type='add', content=edges, bring_to_back=True)]
-
-
-def remove_graph(graph, animate=False, duration=1):
-    drawables = [node.drawable for node in graph.nodes] + [edge.drawable for edge in graph.edges]
-    if animate:
-        animations = [FadeOut(drawable) for drawable in drawables]
-        return [AnimationObject(type='play', content=animations, duration=duration)]
-    return [AnimationObject(type='remove', content=drawables)]
-
-
-def make_unitary(graph):
-    animation_sequence = []
-
-    drawables = graph.remove_all_but_unitary()
-    animations = [FadeOut(drawable) for drawable in drawables]
-    animation_sequence.append(AnimationObject(type='play', content=animations, wait_after=0.5, duration=0.5, bring_to_back=False))
-    return animation_sequence
-
-
 def get_circle(coords, radius, color, secondary_color, border_width=2):
     circle = Dot(point=coords, radius=radius)
     circle.set_fill(color, opacity=1)
@@ -222,6 +181,154 @@ def get_text(text, coords, scale=1):
     return text
 
 
+def draw_graph(graph):
+    """
+    Creates manim animations for drawing a given graph.
+    :returns animations
+    """
+    animation_sequence = []
+    node_drawables = [FadeIn(node.drawable) for node in graph.nodes]
+    edge_drawables = [Create(edge.drawable) for edge in graph.edges]
+    animation_sequence.append(AnimationObject(type='play', content=node_drawables, duration=1, bring_to_front=True))
+    animation_sequence.append(AnimationObject(type='play', content=edge_drawables, duration=1, bring_to_back=True))
+    return animation_sequence
+
+
+def add_graph(graph):
+    nodes = [node.drawable for node in graph.nodes]
+    edges = [edge.drawable for edge in graph.edges]
+    return [AnimationObject(type='add', content=nodes),
+            AnimationObject(type='add', content=edges, bring_to_back=True)]
+
+
+def remove_graph(graph, animate=False, duration=1):
+    drawables = [node.drawable for node in graph.nodes] + [edge.drawable for edge in graph.edges]
+    if animate:
+        animations = [FadeOut(drawable) for drawable in drawables]
+        return [AnimationObject(type='play', content=animations, duration=duration)]
+    return [AnimationObject(type='remove', content=drawables)]
+
+
+def make_unitary(graph):
+    """
+    Creates manim animations for removing all but unitary edges of a given graph.
+    :returns animations
+    """
+    animation_sequence = []
+
+    drawables = graph.remove_all_but_unitary()
+    animations = [FadeOut(drawable) for drawable in drawables]
+    animation_sequence.append(AnimationObject(type='play', content=animations, wait_after=0.5, duration=0.5, bring_to_back=False))
+    return animation_sequence
+
+
+#######################
+#### GEOMETRY STUFF ###
+#######################
+
+class TrackPoint:
+    """
+    A simple point in 2d space, with a direction.
+    This class is used for interpolating the track, hence the name track point.
+    """
+    def __init__(self, coords, direction):
+        self.coords = coords
+        self.direction = direction / np.linalg.norm(direction)
+
+    def __str__(self):
+        return "coords: {}, direction: {}".format(self.coords, self.direction)
+
+    def alter_direction(self, angle):
+        """
+        Alter direction of point by adding angle (in radian)
+        """
+        current_angle = np.arctan2(*self.direction)
+        new_angle = current_angle + angle
+        direction = [np.sin(new_angle), np.cos(new_angle)]
+        self.direction = np.round(direction, decimals=2)
+
+    def as_list(self):
+        """
+        :returns point and direction in one array [x, y, dx, dy]
+        """
+        x, y, _ = self.coords
+        dx, dy = self.direction
+        return [x, y, dx, dy]
+
+
+def transform_coords(coords, shift, scale):
+    x, y = coords
+    x_shift, y_shift = shift
+    return x_shift + x * scale, y_shift + y * scale, 0
+
+
+class GridShowCase:
+    """
+    A helper class for dividing a 2d space into a grid, and keeping track of the positions of all cells.
+    Size and spacing of cells are configurable. The dimensions of the grid are automatically calculated based on a given ratio.
+    """
+    def __init__(self, num_elements, element_dimensions, spacing=[1, 1], space_ratio=[16, 9]):
+        self.grid_dimensions = calculate_grid_dimensions(num_elements, ratio=space_ratio)
+        self.element_width, self.element_height = element_dimensions
+        self.x_spacing, self.y_spacing = spacing
+
+    def get_global_camera_settings(self):
+        """
+        :returns position and size of camera, so that all cells will be in view.
+        position = center of grid
+        size = (width of grid, height of grid)
+        """
+        cols, rows = self.grid_dimensions
+        width = cols * self.element_width + (cols - 1) * self.x_spacing
+        height = rows * self.element_height + (rows - 1) * self.y_spacing
+        size = [width, height]
+        position = np.array(size) / 2
+        return position, size
+
+    def get_zoomed_camera_settings(self, index):
+        """
+        :returns position and size of camera, so that a single cell will be in view.
+        position = center of cell with given index
+        size = (width of cell, height of cell)
+        """
+        bottom_left_x, bottom_left_y = self.get_element_coords(index)
+        position = (bottom_left_x + self.element_width/2, bottom_left_y + self.element_height/2)
+        size = [self.element_width, self.element_height]
+        return position, size
+
+    def get_element_coords(self, index):
+        """
+        :returns position of bottom left corner of cell with given index.
+        """
+        cols, rows = self.grid_dimensions
+        x = index % cols
+        y = np.floor(index / cols)
+        bottom_left_x = x * (self.element_width + self.x_spacing)
+        bottom_left_y = y * (self.element_height + self.y_spacing)
+        return bottom_left_x, bottom_left_y
+
+
+def calculate_grid_dimensions(num_elements, ratio):
+    """
+    Calculates how n elements should be arranged to best fit a given ratio.
+    :returns grid dimensions [width, height]
+    """
+    factor = (ratio[0] * ratio[1])
+    scale = math.sqrt(num_elements / factor)
+    grid_dims = [np.ceil(ratio[0] * scale), np.ceil(ratio[1] * scale)]
+    approx = ratio[0] * scale * ratio[1] * scale
+    # print("Arrange {} elements in ratio {} x {}!".format(num_elements, *ratio))
+    # print("scaling is {scale:.2f}! Because {x_scaled:.2f} x {y_scaled:.2f} = {approx:.2f} ~ {num}".format(scale=scale, x_scaled=scale * ratio[0],
+    #                                                                                                       y_scaled=scale * ratio[1], num=num_elements,
+    #                                                                                                       approx=approx))
+    print("Approximate grid dimensions: {} x {}".format(*grid_dims))
+    return grid_dims
+
+
+#######################
+##### BASIC STUFF #####
+#######################
+
 def print_2d(grid, print_zeros=True):
     height = len(grid[0])
     width = len(grid)
@@ -235,30 +342,6 @@ def print_2d(grid, print_zeros=True):
         print(row)
     print()
 
-
-def get_adjacent(grid, coords):
-    """
-    Get list of booleans indicating which adjacent cells given coords have.
-    :return: list of booleans: [right, up, left, down]
-    """
-    x, y = coords
-    adjacent = []
-    for _x, _y in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-        if x + _x >= len(grid) or y + _y >= len(grid[0]) or x + _x < 0 or y + _y < 0:
-            adjacent.append(False)
-        else:
-            adjacent.append(grid[x + _x][y + _y] > 0)
-
-    return adjacent
-
-
-def is_adjacent(coords1, coords2):
-    x1, y1 = coords1
-    x2, y2 = coords2
-    for _x, _y in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-        if x1 + _x == x2 and y1 + _y == y2:
-            return True
-    return False
 
 if __name__ == '__main__':
     num = 15
