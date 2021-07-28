cd895bbb337e053b4446e15d7fcfd3a1a51cd371
Malte <m1a9l9t7e@gmail.com>
2021-07-29 01:56:03 +0200

    Work on FeatureModel

---
+++ fm/meta.py
@@ -0,0 +1,75 @@
+class Feature:
+    """
+    A FeatureIDE feature
+    """
+
+    def __init__(self, name, sub_features=[], mandatory=None, alternative=None):
+        self.name = name
+        self.mandatory = mandatory
+        self.sub_features = sub_features
+        self.alternative = alternative
+
+    def __str__(self):
+        if len(self.sub_features) > 0:
+            selection = 'xor' if self.alternative else 'or'
+            subs = str([str(sub_feature) for sub_feature in self.sub_features])
+            return '{}: {}{}'.format(self.name, selection, subs)
+        return self.name
+
+
+class BasicStreet(Feature):
+    """
+    A basic abstract street feature
+    """
+
+    def __init__(self, name):
+        super().__init__(name, sub_features=self.get_features(), mandatory=True, alternative=False)
+
+    def get_features(self):
+        lane_markings = Feature('center line markings', sub_features=[Feature('dashed'), Feature('solid'), Feature('double solid')],
+                                mandatory=True, alternative=True)
+        return [lane_markings]
+
+
+class StraightStreet(BasicStreet):
+    """
+    A feature representing a single straight street
+    """
+
+    def get_features(self):
+        parent_features = super().get_features()
+        features = []
+        return parent_features + features
+
+
+class CurvedStreet(BasicStreet):
+    """
+    A feature representing a single curved street
+    """
+
+    def get_features(self):
+        parent_features = super().get_features()
+        features = []
+        return parent_features + features
+
+
+class Intersection(Feature):
+    """
+    A basic abstract street feature
+    """
+
+    def __init__(self, name):
+        super().__init__(name, sub_features=self.get_features(), mandatory=True, alternative=False)
+
+    def get_features(self):
+        right_of_way = Feature('right of way', sub_features=[Feature('yield'), Feature('stop'), Feature('go ahead')],
+                               mandatory=True, alternative=True)
+        turn_direction = Feature('turn direction', sub_features=[Feature('straight'), Feature('left'), Feature('right')],
+                                 mandatory=True, alternative=True)
+        return [right_of_way, turn_direction]
+
+
+if __name__ == '__main__':
+    # feature = StraightStreet('street1')
+    feature = Intersection('intersection1')
+    print(feature)
