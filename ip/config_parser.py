c3a0046b5b93dd003e658dfb17c2da88597c7282
Malte <m1a9l9t7e@gmail.com>
2021-10-28 19:24:45 +0200

    layout parsing

--- ip/config_parser.py
+++ ip/config_parser.py
@@ -1,6 +1,18 @@
 import json
 from pprint import pprint
 
+import numpy as np
+
+from ip.ip_util import QuantityConstraint, ConditionTypes
+from util import TrackProperties
+
+property_name_to_type = {
+    'intersection': TrackProperties.intersection,
+    'turn_90': TrackProperties.turn_90,
+    'turn_180': TrackProperties.turn_180,
+    'straight': TrackProperties.straight,
+}
+
 
 class Config:
     def __init__(self, path=None):
@@ -18,6 +30,42 @@ class Config:
 def parse_layout(layout_dict):
     if layout_dict is None:
         return dict()
+
+    # parse constraints
+    constraints = []
+    if layout_dict.get('constraints') is not None:
+        for constraint in layout_dict['constraints']:
+            if constraint.get('min') is not None and constraint.get('max') is not None:
+                if constraint.get('min') == constraint.get('max'):
+                    q_constraint_equals = QuantityConstraint(
+                        property_type=property_name_to_type[constraint.get('type')],
+                        condition_type=ConditionTypes.equals,
+                        quantity=constraint.get('max')
+                    )
+                    constraints.append(q_constraint_equals)
+                    continue
+            if constraint.get('min') is not None:
+                q_constraint_lower = QuantityConstraint(
+                    property_type=property_name_to_type[constraint.get('type')],
+                    condition_type=ConditionTypes.more_or_equals,
+                    quantity=constraint.get('min')
+                )
+                constraints.append(q_constraint_lower)
+            if constraint.get('max') is not None:
+                q_constraint_upper = QuantityConstraint(
+                    property_type=property_name_to_type[constraint.get('type')],
+                    condition_type=ConditionTypes.less_or_equals,
+                    quantity=constraint.get('max')
+                )
+                constraints.append(q_constraint_upper)
+    layout_dict['constraints'] = constraints
+
+    # Parse solution
+    solution = None
+    if layout_dict.get('solution') is not None:
+        solution = np.array(layout_dict['solution'])
+    layout_dict['solution'] = solution
+
     return layout_dict
 
 
